3ace00a97fe783034f170af1fea6d8c8d1a2c8fe
Ricardo.Ramos <ricardoalramos@gmail.com>
2020-07-12 11:11:30 -0300

    Ajuste id)

--- src/components/Grade.js
+++ src/components/Grade.js
@@ -3,7 +3,7 @@ import GradeDataService from '../services/GradeService';
 
 const Grade = (props) => {
   const initialGradeState = {
-    _id: null,
+    id: null,
     name: '',
     subject: '',
     type: '',
@@ -12,8 +12,8 @@ const Grade = (props) => {
   const [currentGrade, setCurrentGrade] = useState(initialGradeState);
   const [message, setMessage] = useState('');
 
-  const getGrade = (_id) => {
-    GradeDataService.get(_id)
+  const getGrade = (id) => {
+    GradeDataService.get(id)
       .then((response) => {
         setCurrentGrade(response.data);
         console.log(response.data);
@@ -24,8 +24,8 @@ const Grade = (props) => {
   };
 
   useEffect(() => {
-    getGrade(props.match.params._id);
-  }, [props.match.params._id]);
+    getGrade(props.match.params.id);
+  }, [props.match.params.id]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -33,7 +33,7 @@ const Grade = (props) => {
   };
 
   const updateGrade = () => {
-    GradeDataService.update(currentGrade._id, currentGrade)
+    GradeDataService.update(currentGrade.id, currentGrade)
       .then((response) => {
         setMessage('The grade was updated successfully!');
       })
@@ -43,7 +43,7 @@ const Grade = (props) => {
   };
 
   const deleteGrade = () => {
-    GradeDataService.remove(currentGrade._id)
+    GradeDataService.remove(currentGrade.id)
       .then((response) => {
         props.history.push('/grade');
       })
