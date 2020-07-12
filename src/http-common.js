9fc1609ff216f9c0451f24191ece73d19bd264c2
Ricardo.Ramos <ricardoalramos@gmail.com>
2020-07-12 09:07:23 -0300

    Ajuste HTTP-COMMON (2)

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://grades-api-rramos.herokuapp.com/',
+  baseURL: 'https://grades-api-rramos.herokuapp.com',
   headers: {
     'Content-type': 'application/json',
   },
